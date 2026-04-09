bb88a84aee8bf53e6ebd163c17db78974d5fbd8e
ManhHuyVu <manhhuy.vu@student.adelaide.edu.au>
2026-04-09 15:14:59 +0930

    Start making test case for questforge

---
+++ QuestForge.Tests/Player.cs
@@ -0,0 +1,19 @@
+﻿namespace QuestForge.Tests;
+
+public class Player
+{
+    [Fact]
+    public void PlayerInitWithBadName_ThrowExemption()
+    {
+        Assert.Throws<InvalidNameException>(() =>
+        {
+            QuestForge.Player Hero = new QuestForge.Player(null, "The brave hero of the story.");
+        });
+        Assert.Throws<InvalidNameException>(() =>
+        {
+            QuestForge.Player Hero = new QuestForge.Player("   ", "The brave hero of the story.");
+        });
+    }
+
+    public
+}
